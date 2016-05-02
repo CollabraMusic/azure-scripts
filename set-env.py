58da2b4374ec311811c1953d3e59812180a6a985
BRANDON KOBEL <brandon@brandong.local>
2016-05-02 09:10:47 -0400

    Reading JSON from stdin now

--- set-env.py
+++ set-env.py
@@ -4,11 +4,14 @@ import sys, argparse, json
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Create a file with the environment variables')
-    parser.add_argument('-j', '--json', help='JSON with the arguments')
     parser.add_argument('-o', '--output', help='output path')
     args = parser.parse_args(argv)
     
-    json_vars = json.loads(args.json.decode('string_escape'))
+    json_str = ''
+    for line in sys.stdin:
+        json_str += line
+
+    json_vars = json.loads(json_str.decode('string_escape'))
 
     with open(args.output, 'w') as f:
         for k,v in json_vars.iteritems():
