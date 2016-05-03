f9a08be7fb59dfe1f80af03d1efb38cafd02b0e6
BRANDON KOBEL <brandon@brandong.local>
2016-05-03 09:50:51 -0400

    Oh gosh, using the right parameter for the azure file share

--- recording-provision.sh
+++ recording-provision.sh
@@ -64,7 +64,7 @@ chmod +x ./set-env.py
 echo "$ENV" | python ./set-env.py -o /var/www/recording-api/source/.env
 
 chmod +x ./afs-utils-0.1.sh
-bash ./afs-utils-0.1.sh -a "$STORAGE_ACCOUNT_NAME" -p -c -s "$MNT_SHARE_NAME" -b "$MNT_SHARE_NAME" -k  "$STORAGE_ACCESS_KEY"
+bash ./afs-utils-0.1.sh -a "$STORAGE_ACCOUNT_NAME" -p -c -s "$MNT_SHARE_NAME" -b "$MNT_SHARE_DIR" -k  "$STORAGE_ACCESS_KEY"
 
 export HOME=/root PM2_HOME=/root/.pm2
 node /usr/bin/pm2 restart all
