288fd6b7208f2dd0071b8e10307c28164cb1b8fd
BRANDON KOBEL <brandon@brandong.att.net>
2016-05-18 10:54:57 -0400

    Fixing the turn-configure to actually use the username/password

--- turn/turn-configure.sh
+++ turn/turn-configure.sh
@@ -2,6 +2,45 @@
 TURN_USERNAME=
 TURN_PASSWORD=
 
+error()
+{
+    echo "$1" >&2
+    exit 3
+}
+
+log()
+{
+    echo "$1"
+}
+
+require_opt()
+{
+    if [ ! "$1" ];
+    then
+        error "$2 argument is required"
+    fi
+}
+
+while :; do
+    case "$1" in
+        --turn-username)
+            TURN_USERNAME="$2"
+            shift
+            ;;
+        --turn-password)
+            TURN_PASSWORD="$2"
+            shift
+            ;;
+        *)
+            break
+    esac
+
+    shift
+done
+
+require_opt "$TURN_USERNAME" "--turn-username"
+require_opt "$TURN_PASSWORD" "--turn-password"
+
 apt-get update
 apt-get install -y coturn
 
